4def7959dde018ecab79c6f5dad3be9358a650a8
Zoe-997 <thuminh10011997@gmail.com>
2023-12-26 10:59:41 +0700

    register page

--- src/utils/typeForm.ts
+++ src/utils/typeForm.ts
@@ -2,3 +2,11 @@ export interface loginReqBody {
   emailOrUsernameOrPhoneNumber: string
   password: string
 }
+
+export interface registerReqBody {
+  phone: string
+}
+
+export function isVietnamesePhoneNumber(phone: string) {
+  return /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/.test(phone);
+}
